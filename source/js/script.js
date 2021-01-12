06763707f22a62c3e105d5dc7a3ba9afcc134dc3
Squappi <69323875+Squappi@users.noreply.github.com>
2021-01-12 14:08:03 +0300

    End

--- source/js/script.js
+++ source/js/script.js
@@ -5,32 +5,48 @@ const iconEmail = document.querySelector(".form__input--email");
 const iconMobile = document.querySelector(".form__input--mobile");
 const textArea = document.querySelector(".form__input--textarea");
 const button = document.querySelector(".form__button");
+const closerNavBar = document.querySelector('.main-navigation__toggle');
+const navBar = document.querySelector('.main-navigation__wrapper');
 
-function checkName(value) {
-    if (!value || value.length < 0) {
-        iconName.classList.add("modal-error");
-        return false;
+closerNavBar.addEventListener('click', function(evt) {
+    if (closerNavBar.classList.contains('main-navigation__toggle--closed')) {
+        closerNavBar.classList.add('main-navigation__toggle--opened');
+        closerNavBar.classList.remove('main-navigation__toggle--closed');
+        navBar.classList.remove('visually-hidden');
+    } else {
+        closerNavBar.classList.remove('main-navigation__toggle--opened');
+        closerNavBar.classList.add('main-navigation__toggle--closed');
+        navBar.classList.add('visually-hidden');
     }
-    return true;
-}
-button.addEventListener("click", function(evt) {
-    if (checkName(iconName.value) && iconEmail.value && iconWeight.value && iconMobile.value) {
-        localStorage.setItem("login", iconEmail.value);
-        return;
-    }
-    if (!iconEmail.value) {
-        iconEmail.classList.add("modal-error");
-    }
-    if (!iconWeight.value) {
-        iconWeight.classList.add("modal-error");
-    }
-    if (!iconMobile.value) {
-        iconMobile.classList.add("modal-error");
-    }
-    setTimeout(function() {
-        iconName.classList.remove("modal-error");
-        iconEmail.classList.remove("modal-error");
-        iconWeight.classList.remove("modal-error");
-        iconMobile.classList.remove("modal-error");
-    }, 650);
 });
+
+if (iconName && iconWeight && iconAge && iconEmail && textArea && iconMobile && button) {
+    function checkName(value) {
+        if (!value || value.length < 0) {
+            iconName.classList.add("modal-error");
+            return false;
+        }
+        return true;
+    }
+    button.addEventListener("click", function(evt) {
+        if (checkName(iconName.value) && iconEmail.value && iconWeight.value && iconMobile.value) {
+            localStorage.setItem("login", iconEmail.value);
+            return;
+        }
+        if (!iconEmail.value) {
+            iconEmail.classList.add("modal-error");
+        }
+        if (!iconWeight.value) {
+            iconWeight.classList.add("modal-error");
+        }
+        if (!iconMobile.value) {
+            iconMobile.classList.add("modal-error");
+        }
+        setTimeout(function() {
+            iconName.classList.remove("modal-error");
+            iconEmail.classList.remove("modal-error");
+            iconWeight.classList.remove("modal-error");
+            iconMobile.classList.remove("modal-error");
+        }, 650);
+    });
+}
